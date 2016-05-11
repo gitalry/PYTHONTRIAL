0cfd20d1f417dcdf4b664568b850da7d4d061d7a
gitalry <jimgitari@gmail.com>
2016-05-11 05:29:50 +0300

    iniot and views upped

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,4 @@ from config import basedir
 
 lm = LoginManager()
 lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
--- app/views.py
+++ app/views.py
@@ -5,9 +5,10 @@ from .forms import LoginForm
 from .models import User
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
+    user = g.user
+    posts = [
         { 
             'author': {'nickname': 'John'}, 
             'body': 'Beautiful day in Portland!' 
@@ -17,10 +18,12 @@ def index():
             'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template("index.html",
+    return render_template('index.html',
                            title='Home',
                            user=user,
                            posts=posts)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
@@ -34,3 +37,28 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
