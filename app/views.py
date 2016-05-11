789b37eba019efb0347ee42f9168dff580030ec7
gitalry <jimgitari@gmail.com>
2016-05-11 05:22:03 +0300

    updated

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
-
+from .models import User
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,14 +21,15 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
-#the other code from templates
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,
